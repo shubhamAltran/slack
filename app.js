9155a7af91410220140eef2d9402fc010b28416b
PATHAK <sonam.pathak@capgemini.com>
2021-07-22 12:16:05 +0530

    Added Code For Public App Distribution

--- app.js
+++ app.js
@@ -1,79 +1,74 @@
-// const { WebClient } = require('@slack/web-api');
-// const { createEventAdapter } = require('@slack/events-api');
-
-// const slackSigningSecret = '';
-// const slackToken = '';
-// const port = 3000;
-
-// const slackEvents = createEventAdapter(slackSigningSecret);
-// const slackClient = new WebClient(slackToken);
-
-// const conversationId = '';
-
-// // (async () => {
-
-// //   // Post a message to the channel, and await the result.
-// //   // Find more arguments and details of the response: https://api.slack.com/methods/chat.postMessage
-// //   const result = await slackClient.chat.postMessage({
-// //     text: 'Hello world!',
-// //     channel: conversationId,
-// //   });
-
-// //   // The result contains an identifier for the message, `ts`.
-// //   console.log(`Successfully send message ${result.ts} in conversation ${conversationId}`);
-// // })();
-
-// // async function noBotMessages({ message, next }) {
-// //   if (!message.subtype || message.subtype !== 'bot_message') {
-// //     await next();
-// //   }
-// // }
-// slackEvents.on('app_mention', (event) => {
-//   console.log("I cam here")
-//   console.log(`Got message from user ${event.user}: ${event.text}`);
-//   (async () => {
-//     try {
-//       await slackClient.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` })
-//     } catch (error) {
-//       console.log(error.data)
-//     }
-//   })();
-// });
-
-// slackEvents.on('message', (event) => {
-//     (async () => {
-//       try {
-//         await slackClient.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` })
-//       } catch (error) {
-//         console.log(error.data)
-//       }
-//     })();
-//   });
-
-// slackEvents.on('error', console.error);
-
-// slackEvents.start(port).then(() => {
-//   console.log(`Server started on port ${port}`)
-// });
-
 const { App } = require('@slack/bolt');
+let StoreObj = {};
 
 const app = new App({
-  signingSecret: 'enter secret key',
-  token: 'enter token',
+  signingSecret: '',
+  clientId: '',
+  clientSecret: '',
+  stateSecret: '',
+  scopes: ['app_mentions:read', 'calls:read', 'calls:write', 'channels:history', 'channels:read', 'chat:write', 'groups:history', 'im:history', 'im:read', 'im:write', 'incoming-webhook', 'mpim:history', 'team:read'],
+
+  installationStore: {
+    storeInstallation: async (installation) => {
+      // change the line below so it saves to your database
+      if (installation.isEnterpriseInstall && installation.enterprise !== undefined) {
+        // support for org wide app installation
+        StoreObj[installation.enterprise.id] = installation;
+        // console.log(StoreObj);
+        return StoreObj;
+      }
+      if (installation.team !== undefined) {
+        // single team app installation
+        
+        StoreObj[installation.team.id] = installation;
+
+        return StoreObj;
+      }
+      throw new Error('Failed saving installation data to installationStore');
+    },
+    fetchInstallation: async (installQuery) => {
+
+      // change the line below so it fetches from your database
+      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+
+        // org wide app installation lookup
+        return StoreObj[installQuery.enterpriseId];
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation lookup
+
+        return StoreObj[installQuery.teamId];
+      }
+
+      throw new Error('Failed fetching installation');
+    },
+    deleteInstallation: async (installQuery) => {
+
+      // change the line below so it deletes from your database
+      if (installQuery.isEnterpriseInstall && installQuery.enterpriseId !== undefined) {
+        // org wide app installation deletion
+        delete StoreObj[installQuery.enterpriseId];
+        return StoreObj;
+      }
+      if (installQuery.teamId !== undefined) {
+        // single team app installation deletion
+        delete StoreObj[installQuery.teamId];
+        return StoreObj;
+      }
+      throw new Error('Failed to delete installation');
+    },
+  },
 });
 
-/* Add functionality here */
 app.event('message', async ({ event, client }) => {
-  // Do some slack-specific stuff here
-  //enter your channel id
-  if(event.channel === ''){
-  console.log(event.channel)
-  await client.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` });
-}
+
+  if (event.channel_type === 'im') {
+    console.log('inside im');
+
+    await client.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` });
+  }
 });
 app.event('app_mention', async ({ event, client }) => {
-  // Do some slack-specific stuff here
   console.log(event.channel)
   await client.chat.postMessage({ channel: event.channel, text: `Hello <@${event.user}>! :tada:` });
 });
@@ -82,7 +77,7 @@ app.event('error', console.error);
 
 (async () => {
   // Start the app
-  await app.start(process.env.PORT || 3000);
+  await app.start(process.env.PORT || 5000);
 
-  console.log('⚡️ Bolt app is running! on 3000');
+  console.log('⚡️ Bolt app is running! on 5000');
 })();
